1c498e12af1aefbf33be72600e31081d54e4bc60
Nicholas Bruce <saltair93+github@gmail.com>
2022-03-16 13:48:30 -0700

    parameterizes frequencies

--- libs/const/src/lib/dsp.ts
+++ libs/const/src/lib/dsp.ts
@@ -1,16 +1,18 @@
-//Sizes
-export const FULL_SPECTRA_LENGTH = 600000;
-export const FULL_SPECTRA_HZ_PER_BIN = 2000000/FULL_SPECTRA_LENGTH;
-export const DISPLAYED_TIME_LENGTH = 100;
-export const REBINNED_SPECTRA_LENGTH = 1024;
-
 //Limits
+const start_freq = 0
+const bandwidth = 2e9
 export const SPECTRA_MIN_VALUE = 14;
 export const SPECTRA_MAX_VALUE = 18;
 
+//Sizes
+export const FULL_SPECTRA_LENGTH = 600000;
+export const FULL_SPECTRA_HZ_PER_BIN = bandwidth/FULL_SPECTRA_LENGTH;
+export const DISPLAYED_TIME_LENGTH = 100;
+export const REBINNED_SPECTRA_LENGTH = 1024;
+
 //Arrays
 const full_freqs = new Array<number>(FULL_SPECTRA_LENGTH);
-for (let i = 0; i < FULL_SPECTRA_LENGTH; i++) {
+for (let i = start_freq; i < FULL_SPECTRA_LENGTH; i++) {
     full_freqs[i] = i * FULL_SPECTRA_HZ_PER_BIN;
 }
 export const FULL_FREQS = full_freqs;
