d25fab26e42c7f5aacdd12d50079cd891e794ee9
Nicholas Bruce <saltair93+github@gmail.com>
2022-05-06 10:55:39 -0700

    Renames projects, updates docs and scripts

---
+++ apps/web/webpack.config.js
@@ -0,0 +1,17 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (config, context) => {
+  return {
+    ...config,
+    plugins: [
+      ...config.plugins,
+      new CopyPlugin({
+        patterns: [
+          { from: 'src/index.html', to: '' },
+          { from: '../../node_modules/scichart/_wasm/scichart2d.data', to: '' },
+          { from: '../../node_modules/scichart/_wasm/scichart2d.wasm', to: '' },
+        ],
+      }),
+    ],
+  };
+};
