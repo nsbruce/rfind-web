e50fa5b4e1768d7d025b0f588d5531e04b8a1726
Nicholas Bruce <saltair93+github@gmail.com>
2022-05-11 16:15:54 -0700

    Cleans up deliver from gen

--- DataProviders/sim/generator.py
+++ DataProviders/sim/generator.py
@@ -10,25 +10,25 @@ env = {
 
 ##* What to simulate
 # Carriers
-fcs = [25e6, 85e6, 41e6, 900e6, -400e6, -401e6, -402e6]
-phase_incrs = [0]*len(fcs)
+fcs = [0.25e9, 0.85e9, 1e9, 1.2e9, 1.3e9, 1.55e9]
 
 # FM signals
 fms = [
-    (-900e6,200e3), #fc, bw
-    (-915e6,200e3),
-    (-925e6,200e3),
-    (-935e6,200e3),
-    (-950e6,200e3),
-    (-550e6,20e6),
-    (375e6,35e6)
+    (0.3e9,200e3), #fc, bw
+    (0.5e9,10e6),
+    (0.9e9,10e6),
+    (1.1e9,10e6),
+    (1.25e9,35e6),
+    (1.4e9,200e3),
+    (1.5e9,20e6)
 ]
 
-limited_noises=[]
-sweeps=[]
+# Shift everything down because we're going to build a complex spectra
+fcs = [fc-float(env['NX_BANDWIDTH'])/2 for fc in fcs]
+fms = [(fc-float(env['NX_BANDWIDTH'])/2,bw) for fc,bw in fms]
 
 # t_int = const.NBINS/const.FS # so that each fft is one integration
-t_int = int(env['NX_INTEGRATION_RATE'])
+t_int = float(env['NX_INTEGRATION_RATE'])
 nbins = int(env['NX_SPECTRA_LENGTH'])
 
 def FM(fc, BW, t_arr):
@@ -42,21 +42,22 @@ def FM(fc, BW, t_arr):
     return st
 
 
-def integrated_spec_gen(noise_pwr, duration) -> np.ndarray:
+def integrated_spec_gen(noise_pwr) -> np.ndarray:
     t_incr = 0
-
-    while t_incr <= duration:
+    while True:
         t_arr = np.linspace(t_incr, t_incr+t_int, nbins)
 
         output = np.random.normal(0,np.sqrt(noise_pwr), nbins) + 1j*np.random.normal(0,np.sqrt(noise_pwr), nbins)
 
         for fc in fcs:
             output += np.exp(2j*np.pi*fc*t_arr)
+
         for fc, bw in fms:
             output += FM(fc,bw,t_arr)
 
-        output = 10.*np.log10(np.abs(np.fft.fftshift(np.fft.fft(output))))
+        output = np.abs(np.fft.fftshift(np.fft.fft(output)))
 
-        yield output.astype(np.int16)
+        output /= np.max(output)
+
+        yield output
         t_incr += t_int
-    return
