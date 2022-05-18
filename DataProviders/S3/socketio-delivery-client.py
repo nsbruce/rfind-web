35ce7a3ab104ae3d3a8d09af664a0ac2bb90bd96
Nicholas Bruce <saltair93+github@gmail.com>
2022-05-18 14:36:08 -0700

    Updated s3 pusher

--- DataProviders/S3/socketio-delivery-client.py
+++ DataProviders/S3/socketio-delivery-client.py
@@ -1,20 +1,18 @@
-import logging
 import multiprocessing
-import time
 import uuid
+import warnings
 
 import ostore
-import ostore.notifications
-import ostore.notification_daemon
+import rastro.data_feed
+import rastro.time_prefix
+import Calibration
 
 import numpy as np
 from dateutil.parser import isoparse
-import os
 import re
 
 import socketio
 import numpy as np
-import time
 from dotenv import dotenv_values
 
 env = {
@@ -22,13 +20,34 @@ env = {
     **dotenv_values(".env")
 }
 
+def get_keys(storage, queue, key_prefix):
+    generator = rastro.time_prefix.TimePrefixGenerator(5.0, 'seconds')
+
+    keys = rastro.data_feed.keys_from_prefixes(
+        storage,
+        rastro.data_feed.add_prefix(
+            key_prefix, generator.generate_prefixes(multiprocessing.Event())
+        )
+    )
+    for key in keys:
+        queue.put(key)
+
+
 prev_dt = None
 spec    = np.zeros((16, 37500))
 count   = 0
+freq_hz = np.linspace(0e6, 2000e6, 600000)
 
-# Disable the WERKZEUG logging, which is very distracting.
-logging.getLogger('werkzeug').disabled = True
-os.environ['WERKZEUG_RUN_MAIN'] = 'true'
+noise_on_fname = 'noise_on.dat'
+noise_off_fname = 'noise_off.dat'
+
+p_on    = np.fromfile(noise_on_fname, dtype=np.float32)
+p_off   = np.fromfile(noise_off_fname, dtype=np.float32)
+coeffs  = Calibration.get_cal_coeffs(freq_hz, p_on, p_off)
+
+inCalCycle = False
+savedOnCal = True
+savedOffCal = True
 
 sio = socketio.Client(ssl_verify=True) #, logger=True, engineio_logger=True)
 
@@ -53,84 +72,81 @@ object_name = 'test-object-from-script-' + str(uuid.uuid4())
 
 config = ostore.Config.load(env['NX_S3_CONFIG_JSON'])
 storage = ostore.ObjectStore(config)
-topic_config = ostore.TopicConfig(env['NX_DELIVERY_CLIENT_IP'], int(env['NX_S3_PORT']))
-manager = ostore.NotificationManager(config, topic_config)
-manager.add_notification(ostore.NotificationConfig(''))
+key_queue = multiprocessing.Queue()
 
-queue = multiprocessing.Queue()
-daemon = ostore.daemon_factory(topic_config, queue)
-shutdown = multiprocessing.Event()
-multiprocessing.Process(target=daemon, args=(shutdown,)).start()
+key_streamer = multiprocessing.Process(
+    target=get_keys, args=(storage, key_queue, "rfind")
+)
+key_streamer.start()
 
-# make sure the notification daemon has started
-time.sleep(1.0)
+while True:
 
-logging.info(time.asctime())
-
-logging.info('uploading object: %s', object_name)
-storage.put(object_name, ostore.Object(b''))
-logging.info('upload complete')
-time.sleep(1.0)
-if queue.qsize() == 0:
-    logging.info('queue has no data when upload completes')
-    print()
-    print('notifications ARE PROBABLY ASYNCHRONOUS')
-    print()
-else:
-    logging.info('queue has data when upload completes')
-    print()
-    print('notifications ARE PROBABLY NOT ASYNCHRONOUS')
-    print()
-
-def callback_function(key):
-
+    key = key_queue.get()
     match = re.search(env['NX_S3_REGEX'], key)
 
     if match:
 
-        ts = match.group(1)
-        dt = isoparse(ts)
+        timestamp = match.group(1)
+        timeObj = isoparse(timestamp)
         sl = int(match.group(2))
         ch = int(match.group(3))
         if (ch == 1):
             sl += 8 
+        
+        # Decide whether to save new calibration data or use the existing one
+        if timeObj.minute == 1 and not inCalCycle and not savedOnCal:
+            inCalCycle = True
+            savedOnCal = False #Already set to false but still valid
+            savedOffCal = False # Already set to false but still valid
+        if timeObj.minute > 3 and (not savedOnCal or not savedOffCal):
+            raise warnings.Warning('Calibration cycle ended and calibrations were not saved')
 
-        if(prev_dt is not None):
+        if sl in np.arange(2,15):
+            if(prev_dt is not None):
 
-            if (dt-prev_dt).total_seconds() > 0:
+                if (timeObj-prev_dt).total_seconds() > 0:
 
-                if (dt-prev_dt).total_seconds() > 1:
-                    print("-- Dropped Data?")
+                    if (timeObj-prev_dt).total_seconds() > 1:
+                        print("-- Dropped Data?")
 
-                full_spec = np.roll(np.reshape(spec*100, -1), -18750)[60000:-60000].astype('int16').tobytes()
+                    full_spec = np.roll(np.reshape(spec, -1), -18750)
 
-                try:
-                    sio.emit(event='integration', data={'bins': full_spec, 'timestamp':ts}, namespace=env['NX_SOCKETIO_BACKEND_NAMESPACE'])
-                    print("-- Succeeded: " + ts + ' ' + str(count))
-                except Exception as e:
-                    print("-- Failed %s" %(e))
-                prev_dt = dt
-                count = 0
+                    if inCalCycle and timeObj.minute == 1 and not savedOnCal:
+                        # Save uncalibrated spectrum
+                        p_on = full_spec.astype(np.float32)
+                        p_on.tofile(noise_on_fname)
+                        savedOnCal = True
+                    if inCalCycle and timeObj.minute == 2 and not savedOffCal:
+                        p_off = full_spec.astype(np.float32)
+                        p_off.tofile(noise_off_fname)
+                        savedOffCal = True
+                    if timeObj.minute == 3 and inCalCycle:
+                        coeffs = Calibration.get_cal_coeffs(freq_hz, p_on, p_off)
+                        inCalCycle = False
+                        savedOnCal = False # resetting for next time
+                        savedOffCal = False # resetting for next time
 
-            data = np.frombuffer(storage.get(key).data, dtype=np.int64).astype('float32')
-            spec[sl] = 10.*np.log10(data[1250:-1250])
-            count += 1
 
-        else:
+                    full_spec = Calibration.apply_cal(full_spec, coeffs)
+                    full_spec = (1000*np.log10(full_spec/0.001))[60000:-60000].astype('int16').tobytes()
 
-            prev_dt = dt
+                    try:
+                        sio.emit(event='integration', data={'bins': full_spec, 'timestamp':timestamp}, namespace=env['NX_SOCKETIO_BACKEND_NAMESPACE'])
+                        print("-- Succeeded: " + timestamp + ' ' + str(count))
+                    except Exception as e:
+                        print("-- Failed %s" %(e))
+                    prev_dt = timeObj
+                    count = 0
+
+                data = np.frombuffer(storage.get(key).data, dtype=np.int64).astype('float32')
+                spec[sl] = 10.*np.log10(data[1250:-1250])
+                count += 1
+
+            else:
+
+                prev_dt = timeObj
 
 
-try:
-    while True:
-        callback_function(queue.get())
-except KeyboardInterrupt:
-    pass
-
-print('cleaning up')
-shutdown.set()
-storage.delete(object_name)
-manager.cleanup()
 
 
 ##
